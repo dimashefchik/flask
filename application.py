ed0f21293d1371a256209a9a759b4fd29737b913
unknown <dimashefchik@gmail.com>
2022-03-16 01:42:18 +0300

    add app

---
+++ application.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def inxed_1():
+    return 'Hello! The first flask page!!'
+
+@application.route("/help")
+def inxed_2():
+    return '<b>This is  help page!!<b>'
+
+@application.route("/name/<user>")
+def inxed_3(user):
+    return 'Name ' + user.upper()
+
+@application.route("/kvadrat/<x>")
+def inxed_4(x):
+    return str(x) + ' v kvadrate = ' + str(int(x)*int(x))
+
+
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+
+
+# requriments.txt
