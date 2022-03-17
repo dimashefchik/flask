89bdc232b66d40190180347fb23d91184d771779
Dima Shefchik <96975830+dimashefchik@users.noreply.github.com>
2022-03-18 01:09:41 +0300

    Update application.py

--- application.py
+++ application.py
@@ -2,22 +2,8 @@ from flask import Flask
 application = Flask(__name__)
 app = application
 
-@application.route("/")
-def inxed_1():
-    return 'Hello! The first flask page!!'
 
-@application.route("/help")
-def inxed_2():
-    return '<b>This is  help page!!<b>'
+@application.route('/')
+def hello_world():
+    return 'Hello there good sir.'
 
-@application.route("/name/<user>")
-def inxed_3(user):
-    return 'Name ' + user.upper()
-
-@application.route("/kvadrat/<x>")
-def inxed_4(x):
-    return str(x) + ' v kvadrate = ' + str(int(x)*int(x))
-
-if __name__ == "__main__":
-    application.debug = True
-    application.run()
